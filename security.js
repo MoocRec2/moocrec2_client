163c1175be26c48b0eb2cff4f6631964d5d5d7b7
Sameer Mohamed <smrbasil4@gmail.com>
2019-09-01 11:10:21 +0530

    Implemented Proper Login and Page Authentication

--- security.js
+++ security.js
@@ -1,20 +1,48 @@
 
-
-login_flag_name = 'logged_in'
+var baseUrl = 'http://localhost:3000'
 
 function checkLogin() {
-    if (!localStorage.getItem(login_flag_name)) {
-        window.location.href = "login.html"
-    }
+    $.ajax({
+        type: "GET",
+        url: baseUrl + '/users/check-auth',
+        headers: {
+            authorization: 'Bearer ' + localStorage.getItem('token')
+        },
+        success: data => {
+            console.log('SUCCESS PATH')
+        },
+        error: error => {
+            console.log('ERROR PATH')
+            window.location.href = "login.html"
+        }
+    })
 }
 
 function login() {
-    localStorage.setItem(login_flag_name, true)
+    var username = document.getElementById('username').value
+    var password = document.getElementById('password').value
+    $.ajax({
+        type: "POST",
+        url: baseUrl + '/users/login',
+        data: JSON.stringify({ username: username, password: password }),
+        contentType: "application/json; charset=utf-8",
+        success: data => {
+            localStorage.setItem('token', data.token)
+            window.location.href = 'index.html'
+        },
+        error: error => {
+            alert('ERROR')
+        }
+    })
     // TODO: Set profile pic url
     // TODO: Set name
 }
 
 function logout() {
-    localStorage.removeItem(login_flag_name)
+    localStorage.removeItem('token')
     window.location.href = "login.html"
+}
+
+function register() {
+    
 }
